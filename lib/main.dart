2536b5a83da5dfd12ebabe19e9334e2c5ee72e14
Avinash175 <avinashmagar15@gmail.com>
2026-02-09 13:16:43 +0530

    feat: Implement Riverpod for state management, apply custom theming with Google Fonts, and configure CocoaPods for platform dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,38 @@
 import 'package:favourite_place/screens/place_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
+
+final colorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 102, 6, 247),
+  background: Color.fromARGB(255, 56, 49, 66),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        scaffoldBackgroundColor: colorScheme.background,
+        colorScheme: colorScheme,
+        textTheme: GoogleFonts.poppinsTextTheme().apply(
+          bodyColor: Colors.white,
+          displayColor: Colors.white,
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: colorScheme.onPrimaryContainer,
+          foregroundColor: colorScheme.primaryContainer,
+        ),
       ),
       home: PlaceListScreen(),
     );
--- lib/provider/user_places.dart
+++ lib/provider/user_places.dart
@@ -2,5 +2,20 @@ import 'package:favourite_place/model/place.dart';
 import 'package:flutter_riverpod/legacy.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
-  UserPlacesNotifier() : super([]);
+  UserPlacesNotifier() : super(const []);
+
+  void addPlace(Place place) {
+    final newPlace = Place(
+      title: place.title,
+      image: place.image,
+      location: place.location,
+      description: '',
+    );
+    state = [...state, newPlace];
+  }
+
+  final userPlacesProvider =
+      StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+        (ref) => UserPlacesNotifier(),
+      );
 }
